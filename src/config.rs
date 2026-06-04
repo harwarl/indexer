4da521e68cbb5bbab95a91d89acd2a2c06df19c3
harwarl <harwarl87@gmail.com>
2026-06-04 18:55:24 +0100

    feat: add URL validation and configuration for RPC endpoints

--- src/config.rs
+++ src/config.rs
@@ -1,8 +1,15 @@
+use std::env;
+
+use url::Url;
+
 use crate::error::ConfigError;
 
 /// Application configuration loaded from environment variables or other sources.
 #[derive(Debug)]
-pub struct Config {}
+pub struct Config {
+    pub rpc_url: String,
+    pub wss_rpc_url: String,
+}
 
 impl Config {
     /// Creates a new configuration instance using the default configuration source.
@@ -10,8 +17,18 @@ impl Config {
     /// # Returns
     ///
     /// A fully initialized [`Config`] instance.
-    pub fn new() -> Result<Self, ConfigError> {
-        Ok(Self {})
+    pub fn new(
+        rpc_url: String,
+        wss_rpc_url: String
+    ) -> Result<Self, ConfigError> {
+        // Validate the urls
+        validate_url(&rpc_url, "RPC_URL", &["https", "http"])?;
+        validate_url(&wss_rpc_url, "RPC_URL_WSS", &["wss", "ws"])?;
+
+        Ok(Self {
+            rpc_url,
+            wss_rpc_url
+        })
     }
 
     /// Loads configuration values from environment variables.
@@ -27,6 +44,37 @@ impl Config {
     /// depending on the implementation.
     pub fn from_env() -> Result<Self, ConfigError> {
         // Get all the required keys and panic if any fails
-        Self::new()
+        let rpc_url = env::var("RPC_URL")
+            .map_err(|_| ConfigError::MissingVar("RPC_URL is missing".to_string()))?;
+
+        let rpc_url_wss = env::var("RPC_URL_WSS")
+            .map_err(|_| ConfigError::MissingVar("RPC_URL_WSS is missing".to_string()))?;
+
+        Self::new(
+            rpc_url,
+            rpc_url_wss
+        )
+    }
+}
+
+/// Validates that a URL is properly formatted and uses http/https only.
+///
+/// # Errors
+/// Returns `ConfigError::InvalidUrl` if parsing fails or scheme is unsupported.
+fn validate_url(url: &str, field: &str, allowed: &[&str]) -> Result<(), ConfigError> {
+    let parsed = Url::parse(url).map_err(|e| ConfigError::InvalidUrl {
+        field: field.to_string(),
+        message: e.to_string(),
+    })?;
+
+    let scheme = parsed.scheme();
+
+    if allowed.contains(&scheme) {
+        Ok(())
+    } else {
+        return Err(ConfigError::InvalidUrl {
+            field: field.to_string(),
+            message: format!("Expected http/https, got {scheme}"),
+        })
     }
 }
--- src/error.rs
+++ src/error.rs
@@ -6,4 +6,15 @@ pub enum ConfigError {
     /// One or more required environment variables were not found.
     #[error("Missing environment variables: {0}")]
     MissingVar(String),
+
+    /// Invalid Url for one of the environmental variables
+    #[error("Invalid URL format for {field}: {message}")]
+    InvalidUrl { field: String, message: String },
+}
+
+
+#[derive(Debug, Error)]
+pub enum AppError {
+    #[error("Configguration Error: {0}")]
+    Config(#[from] ConfigError)
 }
